5a26f9ba695190d031df17890db49069efcef66f
Oscar El <opelm1@gmail.com>
2023-06-17 21:30:10 +1000

    Move initial page load to its own function in own module

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import "./style.css";
 
-const content = document.getElementById("content");
+import initialLoad from "./initialLoad.js";
 
-const nav = document.createElement("nav");
-nav.classList.add("nav_area");
-
-const main = document.createElement("main");
-main.classList.add("main");
-
-function createNavItem(name, href) {
-    const navItem = document.createElement("div");
-    navItem.classList.add("nav_item", name);
-
-    const navLink = document.createElement("a");
-    navLink.classList.add("nav_link", name);
-    navLink.href = href;
-    navLink.textContent = name.charAt(0).toUpperCase() + name.slice(1);
-
-    navItem.appendChild(navLink);
-    return navItem;
-}
-
-nav.appendChild(createNavItem("home", "http://example.com"));
-nav.appendChild(createNavItem("about", "http://example.com"));
-nav.appendChild(createNavItem("menu", "http://example.com"));
-
-content.appendChild(nav);
-
-content.appendChild(main);
+initialLoad();
